c4eb9bcadb2da9426605e136f99f1fc54d8bbcca
cocio19 <ondrej.tazler@infis.cz>
2025-03-11 09:25:33 +0100

    Add files via upload

---
+++ poznamky.py
@@ -0,0 +1,18 @@
+#git add*
+#git commit -m "idc"
+#git push
+
+proměná = input("váš věk")
+
+proměná = int(proměná) 
+
+#if-podmínka všechno co patří pod if musí být odsazeno 4mezery
+#elif-podmínka odsazeno neni
+#else-když to nesplnuje podmínku zadnou provede se else
+
+#And na spojení 
+#or to nebo to
+
+print("cokoliv chceme vypsat",proměná) #když chceme vypsat text pišeme " " a když chceme promenou tak se odděluje , nebo +
+
+#Ondřej Tázler
