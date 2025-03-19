3f6c89abf37e75d56b19fd4164e50b6c88cdce0e
unknown <ondrej.tazler@infis.cz>
2025-03-19 12:52:49 +0100

    idc

--- ukol_2.py
+++ ukol_2.py
@@ -1,6 +1,13 @@
 cislo1 = float(input("zadej sem desetiný cislo"))
 cislo2 = float(input("zadej sem zase desetiný číslo"))
 
+soucet = cislo1+ cislo2
+rozdil = cislo1- cislo1
+soucin = cislo1*cislo2
+podil = cislo1/cislo2
 
-
+print(f"rozdil je {rozdil}")
+print(f"soucet je {soucet}")
+print(f"soucin je {soucin}")
+print(f"podil je {podil}")
 
